b93879f357c897367fffce6106b44d1b5cde88f3
shwe yee <yonngelay@gmail.com>
2019-06-03 15:32:00 +0800

    Accessing Image Height, Callbacks on Image Load, and Adding Dynamic Spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { span: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const imageHeight = this.imageRef.current.clientHeight;
+    const imageSpans = Math.ceil(imageHeight / 10);
+    this.setState({
+      spans: imageSpans
+    });
+  };
+
   render() {
     const { id, description, urls } = this.props.image;
     return (
-      <div>
-        <img key={id} src={urls.regular} alt={description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img
+          ref={this.imageRef}
+          key={id}
+          src={urls.regular}
+          alt={description}
+        />
       </div>
     );
   }
